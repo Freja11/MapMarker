6357df1e267b7097327a55db2221d067390bb1cc
Amina Bajrektarevic <aminabajrektarevic@gmail.com>
2018-05-16 14:54:38 +0200

    First commit

---
+++ src/components/map.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Markers from './marker';
+import Data from '../db.json';
+
+class Map extends Component {
+	
+	render() {
+		return (
+			<div>
+				<h1>Map</h1>
+			</div>
+		)
+	}
+}
+
+export default Map;
---
+++ src/components/marker.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Markers extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Markers</h1>
+			</div>
+		)
+	}
+}
+
+export default Markers;
